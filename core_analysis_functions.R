e1fdc0fb024ff1811f2f9d8352e325ac87adb4ed
mohammad azemi <mazemi@gmail.com>
2025-07-08 14:51:50 +0430

    add consistency checks before analysis

--- core_analysis_functions.R
+++ core_analysis_functions.R
@@ -50,7 +50,7 @@ summarize_select_one <- function(data, select_one_var, disaggregation_var = NULL
     mutate(
       group_var = disaggregation_var,
       analysis_var = select_one_var,
-      stat = round(n / n_total, 3), 
+      stat = round(n / n_total * 100, 1), 
       analysis_type = "percentage"
     )
   
@@ -69,7 +69,7 @@ summarize_select_multiple <- function(data, prefix, disaggregation_var = NULL) {
   
   for (col in columns) {
     
-    # Assuming "." character is the separator
+    # Asumming "." character is the seperator
     suffix  <- sub(".*\\.", "", col)
     
     data_clean <- data %>%
@@ -80,7 +80,7 @@ summarize_select_multiple <- function(data, prefix, disaggregation_var = NULL) {
       summarise(
         n = sum(!!sym(col) == 1, na.rm = TRUE),
         n_total = n(),
-        stat = round(n / n_total, 3),
+        stat = round(n / n_total * 100, 1),
         .groups = 'drop'
       ) %>%
       mutate(
@@ -121,6 +121,7 @@ summarize_mean <- function(data, numeric_var, disaggregation_var = NULL) {
   return(result)
 }
 
+# Function for numeric variables (mean calculation)
 summarize_median <- function(data, numeric_var, disaggregation_var = NULL) {
   data_filtered <- data %>%
     filter(!is.na(!!sym(numeric_var)))
@@ -142,37 +143,49 @@ summarize_median <- function(data, numeric_var, disaggregation_var = NULL) {
 
 # Main analysis function
 analyse_data <- function(data, param_data, disaggregations) {
+  
+  # removing all empty columns
+  empty_cols <- colnames(data)[colSums(is.na(data)) == nrow(data)]
+  data <- data %>% select(- all_of(empty_cols))
+  
+  # check inconsistencies in the analysis variables 
+  columns_to_check <- param_data$variable
+  missing_cols <- columns_to_check[!columns_to_check %in% names(data)]
+  
+  if (length(missing_cols) != 0) {
+    message("Missing columns detected: ")
+    
+    for(item in missing_cols) {
+      cat(item, "\n")
+    }
+    
+    message("*************************")
+    
+    param_data <- param_data %>% filter(!variable %in% missing_cols)
+  } else {
+    message( "All variables exist in the data frame!")
+  }
+
+  
   results <- list() 
   
   # Loop through each variable in the parameter data
   for (i in 1:nrow(param_data)) {
     variable <- param_data$variable[i]
+    cat("proccessing: ", variable, "\n")
     type <- param_data$type[i]
-    
-    cat("proccessing:", variable, "\n")
     
     # Loop through each disaggregation level
     for (disaggregation in disaggregations) {
-      if (is.na(disaggregation) || is.null(disaggregation)) {
-        if (type == "select_one") {
-          result <- summarize_select_one(data, variable, disaggregation)
-        } else if (type == "select_multiple") {
-          result <- summarize_select_multiple(data, variable, disaggregation)
-        } else if (type == "integer" || type == "decimal") {
-          res1 <- summarize_mean(data, variable, disaggregation)
-          res2 <- summarize_median(data, variable, disaggregation)
-          result <- bind_rows(res1, res2)
-        }
-      }
-      
+
       if (type == "select_one") {
         result <- summarize_select_one(data, variable, disaggregation)
       } else if (type == "select_multiple") {
         result <- summarize_select_multiple(data, variable, disaggregation)
       } else if (type == "integer" || type == "decimal") {
-        res1 <- summarize_mean(data, variable, disaggregation)
-        res2 <- summarize_median(data, variable, disaggregation)
-        result <- bind_rows(res1, res2)
+        result_mean <- summarize_mean(data, variable, disaggregation)
+        result_median <- summarize_median(data, variable, disaggregation)
+        result <- bind_rows(result_mean, result_median)
       }
       
       # Store result in the list, ensuring unique names for each combination
@@ -186,9 +199,7 @@ analyse_data <- function(data, param_data, disaggregations) {
   final_result <- bind_rows(results)
   col_list <- c("group_var", "group_var_value", "analysis_var", "analysis_var_value", "stat", "n", "n_total", "analysis_type")
   final_result <- final_result %>% select(any_of(col_list))
-  
-  final_result$group_var <- ifelse(is.na(final_result$group_var), "No_Disaggregation", final_result$group_var)
-  
+  final_result <- final_result %>% mutate(group_var = ifelse(is.na(group_var), "no_disaggregation", group_var))
   cat("proccessing finished.    ", "\n")
   return(final_result)
 }
